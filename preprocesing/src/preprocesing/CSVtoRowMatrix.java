a62770a675f4f0d4c42454bce3a63ec16946a39a
Sofia Yfantidou <syfantid@csd.auth.gr>
2018-11-29 15:19:20 +0100

    Adds transformation function for Netflix data

--- preprocesing/src/preprocesing/CSVtoRowMatrix.java
+++ preprocesing/src/preprocesing/CSVtoRowMatrix.java
@@ -1,19 +1,11 @@
 package preprocesing;
 
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.*;
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
-import java.util.Set;
-import org.apache.commons.collections.list.TreeList;
 
 public class CSVtoRowMatrix {
   public static class MovieRating {
@@ -29,6 +21,40 @@ public class CSVtoRowMatrix {
     public String toString(){
       return movie+","+rating;
     }
+  }
+
+    /**
+     *
+     * @param path The path to the folder containing the combined Netflix files
+     * @param filename The filename without the number and format e.g. combined_data_
+     * @param parts Number of seperate files e.g. 4
+     * @param fileOut The name of the output file
+     * @throws IOException
+     */
+  public static void transformNetflix(String path, String filename, int parts, String fileOut) throws IOException {
+
+      BufferedWriter writer = new BufferedWriter(new FileWriter(fileOut));
+      String movie="";
+      String userID="";
+      String rating="";
+
+      for(int i=1; i<=parts; i++) { // For all 4 combined files
+          File file = new File(path + filename + i + ".txt");
+          try (BufferedReader br = new BufferedReader(new FileReader(file))) {
+
+              String st;
+              while ((st = br.readLine()) != null) {
+                  if (st.endsWith(":")) {
+                      movie = st.replace(":", "");
+                  } else {
+                      userID = st.split(",")[0];
+                      rating = st.split(",")[1];
+                  }
+                  writer.write(userID + "," + movie + "," + rating + "\n");
+              }
+          }
+      }
+      writer.close();
   }
 
   public static void transform(String fileName, String fileOut) throws IOException {
@@ -75,26 +101,70 @@ public class CSVtoRowMatrix {
         userRatings.add(movieRating);
       }
 
-      for(String id : ratings.keySet()) {
-        LinkedList<String> movieId = new LinkedList<>();
-        LinkedList<String> rating = new LinkedList<>();
+        writeToFile(ratings, writer);
 
-        writer.write(id + "#");
-        for(MovieRating mr : ratings.get(id)) {
-          movieId.add(mr.movie);
-          rating.add(mr.rating);
-        }
-        writer.write(movieId.toString());
-        writer.write("#");
-        writer.write(rating.toString());
-        writer.write("\n");
-      }
-
-      writer.close();
+        writer.close();
     }
   }
 
-  public static void main(String[] args) {
+    /**
+     * Transforms Netflix combined files to COO format
+     * @param fileName The name of the input file without number e.g. combined_data_
+     * @throws IOException
+     */
+    public static void listsNetflix(String fileName) throws IOException {
+        Map<String,List<MovieRating>> ratings = new HashMap<>();
+        BufferedWriter writer = new BufferedWriter(new FileWriter(fileName+".list"));
+
+        for(int i=0; i<=4; i++) { // For each combined file
+            File file = new File(fileName);
+            try (BufferedReader br = new BufferedReader(new FileReader(file + String.valueOf(i) + ".txt"))) {
+                String movie="";
+                String userID="";
+                String rating="";
+                String st;
+                while ((st = br.readLine()) != null) {
+                    //System.out.println(st);
+                    if (st.endsWith(":")) {
+                        movie = st.replace(":", "");
+                    } else {
+                        userID = st.split(",")[0];
+                        rating = st.split(",")[1];
+                    }
+                    MovieRating movieRating = new MovieRating(movie, rating);
+                    List<MovieRating> userRatings = ratings.get(userID);
+                    if (userRatings == null) {
+                        userRatings = new LinkedList<>();
+                        ratings.put(userID, userRatings);
+                    }
+                    userRatings.add(movieRating);
+                }
+            }
+        }
+
+        writeToFile(ratings, writer);
+
+        writer.close();
+    }
+
+    private static void writeToFile(Map<String, List<MovieRating>> ratings, BufferedWriter writer) throws IOException {
+        for(String id : ratings.keySet()) {
+            LinkedList<String> movieId = new LinkedList<>();
+            LinkedList<String> rating = new LinkedList<>();
+
+            writer.write(id + "#");
+            for(MovieRating mr : ratings.get(id)) {
+                movieId.add(mr.movie);
+                rating.add(mr.rating);
+            }
+            writer.write(movieId.toString());
+            writer.write("#");
+            writer.write(rating.toString());
+            writer.write("\n");
+        }
+    }
+
+    public static void main(String[] args) {
     String fileName = "/home/jacamino/Downloads/ml-20m/ratingss.csv";
     try {
       //transform(fileName, fileName+".out");
